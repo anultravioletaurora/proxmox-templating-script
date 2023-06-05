fc2da831f33fb7a91170b2c225584d517000f749
Jack Caulfield <jackcauliflower@gmail.com>
2023-06-05 05:17:31 -0500

    Moving code around

--- script.sh
+++ script.sh
@@ -50,52 +50,58 @@ function create_template() {
     rm $3
 }
 
+function create_debian_templates() {
+
+    ## Debian
+    #Buster (10)
+    wget "https://cloud.debian.org/images/cloud/buster/latest/debian-10-genericcloud-amd64.qcow2"
+    create_template 900 "temp-debian-10" "debian-10-genericcloud-amd64.qcow2"
+    #Bullseye (11)
+    wget "https://cloud.debian.org/images/cloud/bullseye/latest/debian-11-genericcloud-amd64.qcow2"
+    create_template 901 "temp-debian-11" "debian-11-genericcloud-amd64.qcow2" 
+    #Bookworm (12 dailies - not yet released)
+    wget "https://cloud.debian.org/images/cloud/bookworm/daily/latest/debian-12-genericcloud-amd64-daily.qcow2"
+    create_template 902 "temp-debian-12-daily" "debian-12-genericcloud-amd64-daily.qcow2" 
+
+}
+
+function create_ubuntu_templates() {
+    ## Ubuntu
+    #20.04 (Focal Fossa)
+    wget "https://cloud-images.ubuntu.com/releases/focal/release/ubuntu-20.04-server-cloudimg-amd64.img"
+    create_template 910 "temp-ubuntu-20-04" "ubuntu-20.04-server-cloudimg-amd64.img" 
+    #22.04 (Jammy Jellyfish)
+    wget "https://cloud-images.ubuntu.com/releases/22.04/release/ubuntu-22.04-server-cloudimg-amd64.img"
+    create_template 911 "temp-ubuntu-22-04" "ubuntu-22.04-server-cloudimg-amd64.img" 
+    #23.04 (Lunar Lobster) - daily builds
+    wget "https://cloud-images.ubuntu.com/lunar/current/lunar-server-cloudimg-amd64.img"
+    create_template 912 "temp-ubuntu-23-04-daily" "lunar-server-cloudimg-amd64.img"
+
+}
+
+function create_centos_templates() {
+    ## CentOS Stream
+    #Stream 8
+    wget https://cloud.centos.org/centos/8-stream/x86_64/images/CentOS-Stream-GenericCloud-8-20220913.0.x86_64.qcow2
+    create_template 930 "temp-centos-8-stream" "CentOS-Stream-GenericCloud-8-20220913.0.x86_64.qcow2"
+    #Stream 9 (daily) - they don't have a 'latest' link?
+    wget https://cloud.centos.org/centos/9-stream/x86_64/images/CentOS-Stream-GenericCloud-9-20230123.0.x86_64.qcow2
+    create_template 931 "temp-centos-9-stream-daily" "CentOS-Stream-GenericCloud-9-20230123.0.x86_64.qcow2"
+}
+
 
 #Path to your ssh authorized_keys file
 #Alternatively, use /etc/pve/priv/authorized_keys if you are already authorized
 #on the Proxmox system
-export ssh_keyfile=/root/id_rsa.pub
+export ssh_keyfile=/etc/pve/priv/authorized_keys
 #Username to create on VM template
 export username=yuri
 
 #Name of your storage
 export storage=local-lvm
 
-#The images that I've found premade
-#Feel free to add your own
-
-## Debian
-#Buster (10)
-wget "https://cloud.debian.org/images/cloud/buster/latest/debian-10-genericcloud-amd64.qcow2"
-create_template 900 "temp-debian-10" "debian-10-genericcloud-amd64.qcow2"
-#Bullseye (11)
-wget "https://cloud.debian.org/images/cloud/bullseye/latest/debian-11-genericcloud-amd64.qcow2"
-create_template 901 "temp-debian-11" "debian-11-genericcloud-amd64.qcow2" 
-#Bookworm (12 dailies - not yet released)
-wget "https://cloud.debian.org/images/cloud/bookworm/daily/latest/debian-12-genericcloud-amd64-daily.qcow2"
-create_template 902 "temp-debian-12-daily" "debian-12-genericcloud-amd64-daily.qcow2" 
-
-## Ubuntu
-#20.04 (Focal Fossa)
-wget "https://cloud-images.ubuntu.com/releases/focal/release/ubuntu-20.04-server-cloudimg-amd64.img"
-create_template 910 "temp-ubuntu-20-04" "ubuntu-20.04-server-cloudimg-amd64.img" 
-#22.04 (Jammy Jellyfish)
-wget "https://cloud-images.ubuntu.com/releases/22.04/release/ubuntu-22.04-server-cloudimg-amd64.img"
-create_template 911 "temp-ubuntu-22-04" "ubuntu-22.04-server-cloudimg-amd64.img" 
-#23.04 (Lunar Lobster) - daily builds
-wget "https://cloud-images.ubuntu.com/lunar/current/lunar-server-cloudimg-amd64.img"
-create_template 912 "temp-ubuntu-23-04-daily" "lunar-server-cloudimg-amd64.img"
-
 ## Fedora 37
 #Image is compressed, so need to uncompress first
 wget https://download.fedoraproject.org/pub/fedora/linux/releases/37/Cloud/x86_64/images/Fedora-Cloud-Base-37-1.7.x86_64.raw.xz
 xz -d -v Fedora-Cloud-Base-38-1.6.x86_64.raw.xz
-create_template 920 "temp-fedora-38" "Fedora-Cloud-Base-38-1.6.x86_64.raw"
-
-## CentOS Stream
-#Stream 8
-wget https://cloud.centos.org/centos/8-stream/x86_64/images/CentOS-Stream-GenericCloud-8-20220913.0.x86_64.qcow2
-create_template 930 "temp-centos-8-stream" "CentOS-Stream-GenericCloud-8-20220913.0.x86_64.qcow2"
-#Stream 9 (daily) - they don't have a 'latest' link?
-wget https://cloud.centos.org/centos/9-stream/x86_64/images/CentOS-Stream-GenericCloud-9-20230123.0.x86_64.qcow2
-create_template 931 "temp-centos-9-stream-daily" "CentOS-Stream-GenericCloud-9-20230123.0.x86_64.qcow2"
+create_template 9995 "temp-fedora-38" "Fedora-Cloud-Base-38-1.6.x86_64.raw"
